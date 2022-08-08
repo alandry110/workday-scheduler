604e6f33409c25ce6a67e36a33d199dc86a5f0f6
Ashlynn Landry <ashclandry@gmail.com>
2022-08-08 08:43:29 -0400

    start JavaScript

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,38 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
 
-// Save Button 
+// Hours for Local Storage
+
+$("#7am .description").val(localStorage.getItem("7am"));
+$("#8am .description").val(localStorage.getItem("8am"));
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
+
+// SaveBtn Event Listener
+
+$(".saveBtn").on("click", function () {
+    var description = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+
+    localStorage.setItem(description, hour);
+})
+
+
+// Track and change task color 
+
+function scheduleTask() {
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+        var hourId = parseInt($(this).attr("id").split("hour")[1]);
+
+        
+    })
+}
